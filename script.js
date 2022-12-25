5ac741c8775e0000ae08e1784cac00c7e22244fb
Pastila-147 <121392893+Pastila-147@users.noreply.github.com>
2022-12-25 19:23:13 +0400

    Первая версия

---
+++ script.js
@@ -0,0 +1,14 @@
+function ChangePicture() {
+    var i = getRandomIntInclusive(0, 32);
+
+    var imageUrl = `https://chubarich.fvs.app/${i}.png`;
+
+    document.getElementById('photo').src = imageUrl;
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
